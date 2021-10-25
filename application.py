9e7ab2bbae9c6a126021aa7bd05f04858eab5487
Vignesh Varadarajan <vigneshv@gmail.com>
2021-10-25 11:33:18 -0700

    Initial commit

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+application = Flask(__name__)
+Bootstrap(application)
+
+@application.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    application.run()
